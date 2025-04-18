4d1dcba76699e19da9d3aa83ad77338af8c0592b
varshajathan <48399281+varshajathan@users.noreply.github.com>
2025-04-18 16:02:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,15 @@ module "vpc" {
   name                    = "my-tf-vpc"
   auto_create_subnetworks = false
 }
+
+terraform {
+  backend "remote" {
+    organization = "your-org-name"
+
+    workspaces {
+      name = "vpc-project"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
